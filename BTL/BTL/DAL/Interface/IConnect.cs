566095a5ef28fd051c28652bfe21fbc28ffee3c1
202Sasori <65811291+202Sasori@users.noreply.github.com>
2022-04-14 16:30:58 +0700

    Sua login

---
+++ BTL/BTL/DAL/Interface/IConnect.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace BTL.DAL.Interface
+{
+    interface IConnect
+    {
+        string UserID { get; set; }
+        string Password { get; set; }
+        string Database { get; set; }
+        string Server { get; set; }
+        //Trả về chuỗi kết nối
+        string getConectionString();
+        SqlConnection Connect();
+    }
+}
